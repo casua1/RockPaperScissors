49625d29d8d4ddddf69b9df85bb497d4aa2f9866
iD Student <Student@idacademy-mac-8.princeton.edu>
2013-06-24 16:23:02 -0400

    Initial Commit

---
+++ RPS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RPS
+//
+//  Created by iD Student on 6/24/13.
+//  Copyright (c) 2013 Phil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
